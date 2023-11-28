4bc702ba60789a049eb56690b9515df1d7971495
Moa <moa.lovas@gmail.com>
2023-11-28 13:35:01 +0100

    Parkodat med Leo. Börjat med fragmenten och börjat på design.

---
+++ app/src/main/java/com/example/gameboy/GameActivity.kt
@@ -0,0 +1,32 @@
+package com.example.gameboy
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.commit
+import com.example.gameboy.databinding.ActivityGameBinding
+import com.example.gameboy.databinding.ActivityMainBinding
+
+class GameActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityGameBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityGameBinding.inflate(layoutInflater) // Replace YourBindingClass with your actual binding class
+
+        // Use the binding to set up your activity
+        setContentView(binding.root)
+
+        binding.knapp.setOnClickListener(View.OnClickListener {
+            supportFragmentManager.commit {
+                replace(R.id.frame3, PongFragment())
+            }
+        })
+        binding.knapp2.setOnClickListener(View.OnClickListener {
+            supportFragmentManager.commit {
+                replace(R.id.frame3, HockeyFragment())
+            }
+        })
+    }
+}
--- app/src/main/java/com/example/gameboy/MainActivity.kt
+++ app/src/main/java/com/example/gameboy/MainActivity.kt
@@ -1,11 +1,32 @@
 package com.example.gameboy
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.commit
+import com.example.gameboy.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater) // Replace YourBindingClass with your actual binding class
+
+        // Use the binding to set up your activity
+        setContentView(binding.root)
+
+        binding.button5.setOnClickListener(View.OnClickListener {
+            intent = Intent(
+                this, GameActivity::class.java
+
+            )
+            startActivity(intent)
+        })
+
+        supportFragmentManager.commit {
+            add(R.id.frame, PongFragment())
+        }
     }
 }
